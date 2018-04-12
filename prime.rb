e170383e82813041e0a6edd733ce8ebffff1da59
Ayana Bando <ayanaflax@yahoo.com>
2018-04-12 01:57:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+require 'pry'
+def prime?(numbers)
+  prime_number=[]
+  numbers = (-1..110000).to_a
+  if numbers == "(#{numbers} - 1 / #{numbers})"
+    true
+  else
+    false
+    binding.pry
+  end
+end
