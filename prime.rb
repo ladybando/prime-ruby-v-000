cdb6073272e0db4c46ee2f58605196794fb5f946
Ayana Bando <ayanaflax@yahoo.com>
2018-04-12 02:07:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 require 'pry'
 def prime?(numbers)
-  if numbers > 2 || (-1..numbers).to_a.any? {|i| numbers % i == 0}
+  if numbers < 2 || (2...numbers).to_a.any?{|i| numbers % i == 0} # => not prime
     return false
   else
     return true
